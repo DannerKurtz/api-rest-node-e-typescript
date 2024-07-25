4d4d775229bf62473ecbd0e103ae0e785facd46b
Danner Kurtz <kurtz.danner@gmail.com>
2024-07-25 09:23:45 -0300

    adicionado: nova organização de pastas

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from "./server/Server";
 
 server.listen(process.env.PORT || 3232, () => {
-    console.log(`servidor on! PORT: ${process.env.PORT || 3232}`);
+	console.log(`servidor on! PORT: ${process.env.PORT || 3232}`);
 });
