07662d82d4776d90898b838962f7cd3d0e992906
Danner Kurtz <kurtz.danner@gmail.com>
2024-07-29 14:24:59 -0300

    Criando o método de GetAll

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,31 +1,23 @@
 import { Request, RequestHandler, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { validation } from "../../shared/middlewares";
 import * as yup from "yup";
+
+import { validation } from "../../shared/middlewares";
 
 interface ICidade {
   nome: String;
-  estado: String;
-}
-
-interface IFilter {
-  filter: string;
 }
 
 export const createValidation = validation((getSchema) => ({
   body: getSchema<ICidade>(
     yup.object().shape({
       nome: yup.string().required().min(3),
-      estado: yup.string().required().min(2),
-    })
-  ),
-  query: getSchema<IFilter>(
-    yup.object().shape({
-      filter: yup.string().required().min(3),
     })
   ),
 }));
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
-  return res.status(StatusCodes.CREATED).json(req.body);
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .send("Não implementado!");
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -4,6 +4,12 @@ import { CidadesController } from "./../controllers";
 
 const router = Router();
 
+router.get(
+  "/cidades",
+  CidadesController.getAllValidation,
+  CidadesController.getAll
+);
+
 router.post(
   "/cidades",
   CidadesController.createValidation,
