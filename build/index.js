7c46cf4262e284144d12b01dfb4de140574b5fcb
Danner Kurtz <kurtz.danner@gmail.com>
2024-08-01 09:54:49 -0300

    aa

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = require("./server/Server");
-Server_1.server.listen(process.env.PORT || 3232, () => {
-    console.log(`servidor on! PORT: ${process.env.PORT || 3232}`);
-});
