4f0428094660e5389512a2b0f7f8616471071523
Danner Kurtz <kurtz.danner@gmail.com>
2024-08-26 09:03:45 -0300

    feat: adicionado as configurações de ambiente para produção

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -1,5 +1,6 @@
 import { Knex } from "knex";
 import path from "path";
+import { number } from "yup";
 
 export const development: Knex.Config = {
   client: "sqlite3",
@@ -35,5 +36,19 @@ export const test: Knex.Config = {
 };
 
 export const production: Knex.Config = {
-  ...development,
+  client: "pg",
+  migrations: {
+    directory: path.resolve(__dirname, "..", "migrations"),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, "..", "seeds"),
+  },
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    ssl: { rejectUnauthorized: false },
+  },
 };
